2cfb6d9ced2a29d61c27da03538b474c853b36aa
sami757 <gosu747@hotmail.com>
2014-11-08 02:14:21 -0800

    installed SQLite package

--- HolidayMailer/HolidayMailer/DatabaseManager.cs
+++ HolidayMailer/HolidayMailer/DatabaseManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Data;
 using System.IO;
 
 
@@ -15,44 +16,82 @@ namespace HolidayMailer
         private String connectionString = "Data Source=HolidayMailer.db;Version=3;Compress=true";
         private SQLiteConnection connection;
         private SQLiteCommand command;
-        private bool isNew;
 
         public DatabaseManager()
         {
-            createDatabase();
-            connectDatabase();
-            
-            if( isNew ) 
+            if( createDatabase() ) 
                 createTable();
         }
 
-        private void createDatabase()
+        public void add(string lname, string fname, string email, bool lastYear)
+        {
+            string sql = "INSERT INTO default_user (LastName, FirstName, Email, LastYear)"
+                        + "VALUES (\'" + lname + "\', \'" + fname + "\', \'" + email + "\', \'"
+                        + lastYear + "\');";
+
+            executeQuery(sql);
+        }
+
+        public void delete(string email)
+        {
+            string sql = "DELETE FROM default_user WHERE Email = \'" + email + "\';";
+
+            executeQuery(sql);
+        }
+
+        public void clearDB()
+        {
+            string sql = "DELETE * FROM default_user;";
+
+            executeQuery(sql);
+        }
+        public void retrieveContacts()
+        {
+            command = new SQLiteCommand("SELECT * FROM default_user;");
+            SQLiteDataAdapter da = new SQLiteDataAdapter();
+            DataTable dt = new DataTable();
+            da.SelectCommand = command;
+            da.Fill(dt);
+
+        }
+
+        private bool createDatabase()
         {
             if( !File.Exists(dbfilename) )
             {
                 SQLiteConnection.CreateFile(dbfilename);
-                isNew = true;
+                return true;
             }
-            else
-                isNew = false;
+            return false;
         }
 
-        private void connectDatabase()
+        private void setConnection()
         {
             connection = new SQLiteConnection(connectionString);
+        }
+        
+        private void executeQuery(string queryTxt)
+        {
+            setConnection();
             connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = queryTxt;
+            command.ExecuteNonQuery();
+            connection.Close();
         }
 
         private void createTable()
         {
             string sql = "CREATE TABLE default_user ("
-                         + "LastName VARCHAR(30),"
-                         + "FirstName VARCHAR(30),"
-                         + "Email VARCHAR(100),"
-                         + "LastYear BOOLEAN)";
+                         + "LastName VARCHAR(30) NOT NULL,"
+                         + "FirstName VARCHAR(30) NOT NULL,"
+                         + "Email VARCHAR(100) PRIMARY KEY NOT NULL,"
+                         + "LastYear BOOLEAN);";
 
-            command = new SQLiteCommand(sql, connection);
-            command.ExecuteNonQuery();
+            executeQuery(sql);
         }
+
+        
+
     }
 }
