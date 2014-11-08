1c4f4a0290123a2cfbfa7ba686a65aa3214a4c67
sami757 <gosu747@hotmail.com>
2014-11-08 01:19:59 -0800

    created DatabaseManager

---
+++ HolidayMailer/HolidayMailer/DatabaseManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace HolidayMailer
+{
+    class DatabaseManager
+    {
+        private String dbfilename = "HolidayMailer.db";
+        private String connectionString = "Data Source=HolidayMailer.db;Version=3;Compress=true";
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
+        private bool isNew;
+
+        public DatabaseManager()
+        {
+            createDatabase();
+            connectDatabase();
+            
+            if( isNew ) 
+                createTable();
+        }
+
+        private void createDatabase()
+        {
+            if( !File.Exists(dbfilename) )
+            {
+                SQLiteConnection.CreateFile(dbfilename);
+                isNew = true;
+            }
+            else
+                isNew = false;
+        }
+
+        private void connectDatabase()
+        {
+            connection = new SQLiteConnection(connectionString);
+            connection.Open();
+        }
+
+        private void createTable()
+        {
+            string sql = "CREATE TABLE default_user ("
+                         + "LastName VARCHAR(30),"
+                         + "FirstName VARCHAR(30),"
+                         + "Email VARCHAR(100),"
+                         + "LastYear BOOLEAN)";
+
+            command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
